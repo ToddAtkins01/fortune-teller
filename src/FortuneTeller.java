7cba6d67f84bc8782c083a86cb600dd6064d1faf
Todd Atkins <toddatkins01@gmail.com>
2017-08-27 13:21:15 -0400

    part 3 up to bank balance

--- src/FortuneTeller.java
+++ src/FortuneTeller.java
@@ -9,29 +9,31 @@ public class FortuneTeller {
 		// Part 1
 
 		// Ask the user for the user’s first name.
+		// Ask the user for the user’s last name.
+		// Ask the user for the user’s age.
+		// Ask the user for the user’s birth month (as an 'int').
+		// Ask the user for the user’s favorite ROYGBIV color.
+		// If the user does not know what ROYGBIV is, ask the user to enter “Help” to
+		// get a list of the ROYGBIV colors.
+		// Ask the user for the user's number of siblings.
+
 		System.out.println("Hello! Please enter your first name.");
 		String firstName = input.next();
 		String upperFirstName = firstName.substring(0, 1).toUpperCase() + firstName.substring(1);
 
-		// Ask the user for the user’s last name.
 		System.out.println("Thank you! Please enter your last name.");
 		String lastName = input.next();
 		String upperLastName = lastName.substring(0, 1).toUpperCase() + lastName.substring(1);
 
-		// Ask the user for the user’s age.
 		System.out.println("Awesome! Now, enter your age please.");
 		int age = input.nextInt();
 
-		// Ask the user for the user’s birth month (as an 'int').
 		System.out.println("You're doing GREAT! Now, please enter your birth month in xx format.\n"
 				+ " (Example: March would be 03.)");
 		int birthMonth = input.nextInt();
 
-		// Ask the user for the user’s favorite ROYGBIV color.
 		System.out.println("Ok almost done. What is your favorite ROYGBIV color?");
 
-		// If the user does not know what ROYGBIV is, ask the user to enter “Help” to
-		// get a list of the ROYGBIV colors.
 		System.out.println("(Not sure what ROYGBIV is? Just type \"Help\" to find out.)");
 		String favoriteColor = input.next();
 		while (favoriteColor.equalsIgnoreCase("Help")) {
@@ -49,7 +51,6 @@ public class FortuneTeller {
 
 		}
 
-		// Ask the user for the user's number of siblings.
 		System.out.println(
 				"Alright! " + upperFirstName + " Last question....I promise. ;-). How many siblings do you have?");
 		int numberOfSiblings = input.nextInt();
@@ -66,11 +67,13 @@ public class FortuneTeller {
 		// user's age is odd or even.
 		/// (condition) If the user's age is… (example) then the user will retire in…
 		// odd 25 years
+		// even 16 years
 		if (age % 2 != 0) {
-			String ageIsOdd = "25 years";
+			age = (25);
+
 		} else {
-			// even 16 years
-			String ageIsEven = "16 Years";
+
+			age = (16);
 		}
 
 		// Vacation Home Location
@@ -85,7 +88,20 @@ public class FortuneTeller {
 		// 3 Bora Bora
 		// greater than three Cape Cora, Florida
 		// less than zero Lake Chapala, Jalisco
-		//
+
+		if (numberOfSiblings == 0) {
+			String vacationHome = "Savannah, Ga";
+		} else if (numberOfSiblings == 1) {
+			String vacationHome = "Abruzzo, Italy";
+		} else if (numberOfSiblings == 2) {
+			String vacationHome = "Zermatt, Switzerland";
+		} else if (numberOfSiblings == 3) {
+			String vacationHome = "Bora Bora";
+		} else if (numberOfSiblings >= 3) {
+			String vacationHome = "Cora, Florida";
+		} else {
+			String vacationHome = "Lake Chapala, Jalisco";
+		}
 
 		// Mode of Transporation
 		/// The user's mode of transportation will determined by the user's favorite
@@ -99,6 +115,23 @@ public class FortuneTeller {
 		// blue Bentley Mulsanne
 		// indigo Honda Odyssey
 		// violet Hyperloop
+		if (favoriteColor.equalsIgnoreCase("red")) {
+			String modeOfTransportation = "Huffy";
+		} else if (favoriteColor.equalsIgnoreCase("orange")) {
+			String modeOfTransportation = "Ford Pinto";
+		} else if (favoriteColor.equalsIgnoreCase("yellow")) {
+			String modeOfTransportation = "Uber/Lyft";
+		} else if (favoriteColor.equalsIgnoreCase("green")) {
+			String modeOfTransportation = "Teleport";
+		} else if (favoriteColor.equalsIgnoreCase("blue")) {
+			String modeOfTransportation = "Bentley Mulsanne";
+		} else if (favoriteColor.equalsIgnoreCase("indigo")) {
+			String modeOfTransportation = "Honda Odysseye";
+		} else if (favoriteColor.equalsIgnoreCase("violet")) {
+			String modeOfTransportation = "Hperloop";
+		} else {
+			String modeOfTransportation = "Rocket to the moon for not answering correctly.";
+		}
 		//
 		// Bank Balance
 		/// The user's bank balance at retirement will be based on the user's birth
@@ -112,15 +145,15 @@ public class FortuneTeller {
 		// 9-12 $2,145,203.97
 		// anything else -$376,983.83
 		if (birthMonth == 1 || birthMonth == 2 || birthMonth == 3) {
-			String vacationHome = "$5,999,123.56";
+			String bankBalance = "$5,999,123.56";
 		} else if (birthMonth == 4 || birthMonth == 5 || birthMonth == 6) {
-			String vacationHome = "$265,395.99";
+			String bankBalance = "$265,395.99";
 		} else if (birthMonth == 7 || birthMonth == 8 || birthMonth == 9) {
-			String vacationHome = "$1";
+			String bankBalance = "$1";
 		} else if (birthMonth == 10 || birthMonth == 11 || birthMonth == 12) {
-			String vacationHome = "$$2,145,203.97";
+			String bankBalance = "$2,145,203.97";
 		} else {
-			String vacationHome = "-$376,983.83";
+			String bankBalance = "-$376,983.83";
 		}
 		// Part 3
 		//
@@ -131,6 +164,10 @@ public class FortuneTeller {
 		// a vacation home in *[location]*, and travel by *[mode of transporation]*.
 		// Your program should be able to process input whether a user enters capital or
 		// lowercase letters.
+		System.out.println(upperFirstName + " " + upperLastName + " will retire in " + age + " years with "); + bankBalance
+		// + " in the bank, a vacation home in " + vacationHome + ", and travel by " +
+		// modeOfTransportation + ".");
+
 		input.close();
 
 	}
