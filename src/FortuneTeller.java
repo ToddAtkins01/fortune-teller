2982a8fccf4d045875f164b5bf112a8d9a2b12f4
Todd Atkins <toddatkins01@gmail.com>
2017-08-27 11:55:05 -0400

    Sunday

--- src/FortuneTeller.java
+++ src/FortuneTeller.java
@@ -65,34 +65,40 @@ public class FortuneTeller {
 		// The user's number of years until retirement will be based on whether the
 		// user's age is odd or even.
 		/// (condition) If the user's age is… (example) then the user will retire in…
-		// odd 14 years
-		// even 12 years
-		//
+		// odd 25 years
+		if (age % 2 != 0) {
+			String ageIsOdd = "25 years";
+		} else {
+			// even 16 years
+			String ageIsEven = "16 Years";
+		}
+
 		// Vacation Home Location
 		// The location of the user's vacation home will be based on how many siblings
 		// the user has. If the user enters a number less than zero, give the user a bad
 		// location!
 		/// (condition) If the user's number of siblings is… (example) then the user's
 		// vacation home will be in…
-		// 0 Boca Raton, FL
-		// 1 Nassau, Bahamas
-		// 2 Ponta Negra,Brazil
-		// 3 Portland, Oregon
-		// greater than three Baton Rouge, LA
-		// less than zero Chernobyl, Ukraine
+		// 0 Savannah, Ga
+		// 1 Abruzzo, Italy
+		// 2 Zermatt, Switzerland
+		// 3 Bora Bora
+		// greater than three Cape Cora, Florida
+		// less than zero Lake Chapala, Jalisco
 		//
+
 		// Mode of Transporation
 		/// The user's mode of transportation will determined by the user's favorite
 		// color.
 		/// (condition) If the user's favorite color is… (example) then the user's mode
 		// of transportation will be…
-		// red Maserati
-		// orange stallion
-		// yellow chariot
-		// green taxi
-		// blue rickshaw
-		// indigo motor scooter
-		// violet flying saucer
+		// red Huffy
+		// orange Ford Pinto
+		// yellow Uber/Lyft
+		// green Teleport
+		// blue Bentley Mulsanne
+		// indigo Honda Odyssey
+		// violet Hyperloop
 		//
 		// Bank Balance
 		/// The user's bank balance at retirement will be based on the user's birth
@@ -100,11 +106,22 @@ public class FortuneTeller {
 		// user's balance will be $0.00.
 		/// (condition) If the user's birth month is… (example) The user's balance will
 		// be…
-		// 1-4 $256,000.76
-		// 5-8 $3,687,105.42
-		// 9-12 $86.23
-		// anything else $0.00
-
+		// 1-3 $5,999,123.56
+		// 4-6 $265,395.99
+		// 6-8 $1
+		// 9-12 $2,145,203.97
+		// anything else -$376,983.83
+		if (birthMonth == 1 || birthMonth == 2 || birthMonth == 3) {
+			String vacationHome = "$5,999,123.56";
+		} else if (birthMonth == 4 || birthMonth == 5 || birthMonth == 6) {
+			String vacationHome = "$265,395.99";
+		} else if (birthMonth == 7 || birthMonth == 8 || birthMonth == 9) {
+			String vacationHome = "$1";
+		} else if (birthMonth == 10 || birthMonth == 11 || birthMonth == 12) {
+			String vacationHome = "$$2,145,203.97";
+		} else {
+			String vacationHome = "-$376,983.83";
+		}
 		// Part 3
 		//
 		// Display the user's fortune in this format:
@@ -114,6 +131,7 @@ public class FortuneTeller {
 		// a vacation home in *[location]*, and travel by *[mode of transporation]*.
 		// Your program should be able to process input whether a user enters capital or
 		// lowercase letters.
+		input.close();
 
 	}
 
